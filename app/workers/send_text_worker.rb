9382e4b3cabb6193b19bb6bddba3769c0e36c630
Monden Akihiko <mondenakihiko@Monden-no-MacBook-Pro.local>
2016-10-22 12:57:10 +0900

    bot1

--- app/workers/send_text_worker.rb
+++ app/workers/send_text_worker.rb
@@ -2,15 +2,18 @@ class SendTextWorker
   @queue = :default # queue名を指定
 
   def self.perform(channel_secret, channel_token, mid, text)
+    Rails.logger.debug("[SendTextWorker] mid=#{mid} text=${text}")
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = channel_secret
       config.channel_token = channel_token
     }
+    Rails.logger.debug("[SendTextWorker] @client=#{@client.inspect}")
 
     message = {
       type: 'text',
       text: text
     }
+    Rails.logger.debug("[SendTextWorker] message=#{message.inspect}")
     @client.push_message(mid, message)
   end
 
