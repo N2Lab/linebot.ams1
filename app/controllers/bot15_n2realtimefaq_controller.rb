02b90ed17717e27e933ff97ab42cc4ef2c6c9996
MondenAkihiko <a.monden@opt.ne.jp>
2016-10-29 08:23:30 +0900

    bot15

--- app/controllers/bot15_n2realtimefaq_controller.rb
+++ app/controllers/bot15_n2realtimefaq_controller.rb
@@ -58,7 +58,7 @@ class Bot15N2realtimefaqController < ApplicationController
     profile = get_profile(@client, mid)
     
     # save
-    event_hash = event.to_json.to_hash
+    event_hash = JSON.parse(event.to_json)
     event_hash["profile"] = profile
     
     attr = Attr.save(BOT_ID, mid, 1, 1, event_hash.to_json) # NO=1 を受信msg
