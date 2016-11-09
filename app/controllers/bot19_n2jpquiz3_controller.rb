5040adf977a21a411d60ef587afe17de04cd3e48
MondenAkihiko <a.monden@opt.ne.jp>
2016-11-09 22:54:43 +0900

    bot19

--- app/controllers/bot19_n2jpquiz3_controller.rb
+++ app/controllers/bot19_n2jpquiz3_controller.rb
@@ -143,7 +143,7 @@ https://www.facebook.com/n2lab.inc/
   
   # 問題の画像を配信
   def create_qa_img(answer_pref)
-    url = "http://img.n2bot.net/bot19/pref/pref#{answer_pref}.png"
+    url = "https://img.n2bot.net/bot19/pref/pref#{answer_pref}.png"
     {
       type: "image",
       originalContentUrl: url,
