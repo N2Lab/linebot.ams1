caf2961b60d6dcde410da825d7fa02eda386ac31
Akihiko Monden <akihikomonden@AkihikonoMacBook-Pro.local>
2016-11-23 08:00:04 +0900

    Pullgit

--- app/controllers/pullgit_controller.rb
+++ app/controllers/pullgit_controller.rb
@@ -2,5 +2,6 @@ class PullgitController < ApplicationController
   def index
     Rails.logger.info("PullgitController start")
     Rails.logger.info(`/var/www/sh/pull.sh`)
+    render text: ""
   end
 end
