a2704641e924546cb91e4fc466960efc9132bf11
Akihiko Monden <akihikomonden@AkihikonoMacBook-Pro.local>
2016-11-23 17:33:12 +0900

    bot26

--- app/controllers/bot26_n2bokashu_controller.rb
+++ app/controllers/bot26_n2bokashu_controller.rb
@@ -94,7 +94,8 @@ class Bot26N2bokashuController < ApplicationController
 #    ["Gradiation", "Magick::Image.new(100,100,Magick::GradientFill.new(0, 0, 0, 100, '#900', '#000')) { self.background_color = 'red'; self.format = 'PNG' }"], # 左右黒
 #    ["Gradiation", "Magick::Image.new(img.columns,img.rows,Magick::GradientFill.new(0, 0, img.columns, 0, '#fff', '#000')) { self.background_color = 'white'; self.format = 'PNG' }"], # 上白>下黒
     # ぼかしマスクあり
-    ["Gradiationmask", "img.add_compose_mask(Magick::Image.new(img.columns,img.rows,Magick::GradientFill.new(0, 0, img.columns, 0, '#fff', '#000')) { self.background_color = 'white'; self.format = 'PNG' })", "img.blur_image(10.0, 5.0)"], # 上白>下黒マスク, ぼかし
+    ["GradMask 上ぼけ 下なし", "img.add_compose_mask(Magick::Image.new(img.columns,img.rows,Magick::GradientFill.new(0, 0, img.columns, 0, '#fff', '#000')) { self.background_color = 'white'; self.format = 'PNG' })", "img.blur_image(30.0, 5.0)"], # 上白>下黒マスク, ぼかし
+    ["GradMask 下ぼけ 上なし", "img.add_compose_mask(Magick::Image.new(img.columns,img.rows,Magick::GradientFill.new(0, 0, 0,    img.rows, '#fff', '#000')) { self.background_color = 'white'; self.format = 'PNG' })", "img.blur_image(30.0, 5.0)"], # 上白>下黒マスク, ぼかし
 
     # 合成 > あとで
 #    ["夢の中", "img.composite(img, 30, 30, Magick::OverCompositeOp)"], # 30,30右下に重ねる
