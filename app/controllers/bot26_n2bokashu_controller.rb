8b4bbc9f0425841574075f61e610c9cebe9fed7c
Akihiko Monden <akihikomonden@AkihikonoMacBook-Pro.local>
2016-11-23 15:35:01 +0900

    bot26

--- app/controllers/bot26_n2bokashu_controller.rb
+++ app/controllers/bot26_n2bokashu_controller.rb
@@ -117,7 +117,11 @@ class Bot26N2bokashuController < ApplicationController
 
     # blur sample
     new_img = org_img.blur_image(20.0, 10.0) # ブラーエフェクトを適用　戻り値に新しい画像が戻るよ
-    new_f = File.open("/tmp/cv_#{msg_id}","wb"){|f| f << new_img.to_blob }
+    # new_f = File.open("/tmp/cv_#{msg_id}","wb"){|f| f << new_img.to_blob }
+    new_f = Tempfile.open("content_#{msg_id}_2")
+    new_f.binmode
+    new_f.write(new_img.to_blob)
+
     @uploader.store!(new_f)
     Rails.logger.info("new_f = #{new_f.inspect}")
     Rails.logger.info("store_res = #{store_res.inspect}")
