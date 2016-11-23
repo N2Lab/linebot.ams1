13826c439c14606147f177c91b19a6ee34f5003a
Akihiko Monden <akihikomonden@AkihikonoMacBook-Pro.local>
2016-11-23 17:20:25 +0900

    Bot26

--- app/controllers/bot26_n2bokashu_controller.rb
+++ app/controllers/bot26_n2bokashu_controller.rb
@@ -91,7 +91,7 @@ class Bot26N2bokashuController < ApplicationController
 
     # マスクxぼかし を試す
     # まずはグラディエーションを作成
-    ["Gradiation", "Magick::Image.new(100,100,Magick::GradientFill.new(0, 0, 0, 100, '#900', '#000')) { self.background_color = 'red' }"],
+    ["Gradiation", "Magick::Image.new(100,100,Magick::GradientFill.new(0, 0, 0, 100, '#900', '#000')) { self.background_color = 'red'; self.format = 'PNG' }"],
 
     # 合成 > あとで
 #    ["夢の中", "img.composite(img, 30, 30, Magick::OverCompositeOp)"], # 30,30右下に重ねる
