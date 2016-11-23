6dd540629e12735d871fc5b110073c9b36f9759d
Akihiko Monden <akihikomonden@AkihikonoMacBook-Pro.local>
2016-11-23 16:51:43 +0900

    Bot26

--- app/controllers/bot26_n2bokashu_controller.rb
+++ app/controllers/bot26_n2bokashu_controller.rb
@@ -88,14 +88,18 @@ class Bot26N2bokashuController < ApplicationController
     # ["おっとり", "img.blur_image(10.0, 10.0)"],
     # ["オールドレンズA", "img.radial_blur(20.0)"],
     # ["オールドレンズB", "img.radial_blur(40.0)"],
-    # selective_blur_channel
-    ["ポートレート1", "img.selective_blur_channel(10.0, 5.0, 30)"],
-    ["ポートレート2", "img.selective_blur_channel(10.0, 5.0, 50)"],
-    ["ポートレート3", "img.selective_blur_channel(10.0, 5.0, 60)"],
-    ["ポートレート4", "img.selective_blur_channel(10.0, 5.0, 70)"],
-    ["ポートレート5", "img.selective_blur_channel(10.0, 5.0, 80)"],
-    ["ポートレート6", "img.selective_blur_channel(10.0, 10.0, 30)"],
-    ["ポートレート7", "img.selective_blur_channel(10.0, 10.0, 50)"],
+
+    # 合成
+    ["夢の中", "img.radial_blur(20.0)", "img.composite_image(img, 30, 30, Magick::OverCompositeOp)"],
+
+    # selective_blur_channel > 保留 全体がぼけてしまう
+    # ["ポートレート1", "img.selective_blur_channel(10.0, 5.0, 30)"],
+    # ["ポートレート2", "img.selective_blur_channel(10.0, 5.0, 50)"],
+    # ["ポートレート3", "img.selective_blur_channel(10.0, 5.0, 60)"],
+    # ["ポートレート4", "img.selective_blur_channel(10.0, 5.0, 70)"],
+    # ["ポートレート5", "img.selective_blur_channel(10.0, 5.0, 80)"],
+    # ["ポートレート6", "img.selective_blur_channel(10.0, 10.0, 30)"],
+    # ["ポートレート7", "img.selective_blur_channel(10.0, 10.0, 50)"],
     # ["夢の国5", "img.blur_image(20.0, 10.0)"],
     # ["夢の国6", "img.blur_image(20.0, 10.0)"],
     # ["夢の国7", "img.blur_image(20.0, 10.0)"],
