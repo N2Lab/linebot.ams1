52903e5a4be754557d57a749979ca061440bf740
MondenAkihiko <a.monden@opt.ne.jp>
2018-01-07 11:56:53 +0900

    add get refresh token

--- app/controllers/google_cal_controller.rb
+++ app/controllers/google_cal_controller.rb
@@ -1,5 +1,12 @@
 # Google Calendar API controller
 # 
+#
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+
+require 'fileutils'
+
 class GoogleCalController < ApplicationController
 
   require 'net/http'
@@ -9,6 +16,18 @@ class GoogleCalController < ApplicationController
   def callback
     Rails.logger.debug("params=#{params}")
     Rails.logger.debug("request.raw_post=#{request.raw_post}")
-    render text: ""
+
+    code = params[:code] # 短期間トークン If the user approves, then Google gives your application a short-lived access token
+
+    # 長期間トークンを取得
+    uri = URI('https://accounts.google.com/o/oauth2/revoke')
+    get_token_params = { :token => auth_client.access_token }
+    uri.query = URI.encode_www_form(get_token_params)
+    response = Net::HTTP.get(uri)
+
+    Rails.logger.debug("get token response.body=#{response.body} response=#{response}")
+
+
+    render text: "#{response.inspect}"
   end
 end
