3665785aa2b9f91cc48e305335543393bdd6e535
MondenAkihiko <a.monden@opt.ne.jp>
2018-01-07 12:31:00 +0900

    add get refresh token

--- app/controllers/google_cal_controller.rb
+++ app/controllers/google_cal_controller.rb
@@ -21,9 +21,26 @@ class GoogleCalController < ApplicationController
     code = params[:code] # 短期間トークン If the user approves, then Google gives your application a short-lived access token
 
     # 長期間トークンを取得
-    uri = URI('https://accounts.google.com/o/oauth2/revoke')
-    get_token_params = { :token => code }
-    uri.query = URI.encode_www_form(get_token_params)
+    # 以下はコマンドラインでもOK
+    # 参考 https://qiita.com/giiko_/items/b0b2ff41dfb0a62d628b
+    # 自己入力部分
+
+    # 自動部分
+    q_hash = {
+      client_id: "453886901491-evvsmmc5ei10tss4nlqab3f72k0ddmlh.apps.googleusercontent.com",
+      client_secret: "_teiidDLMuEEyKN4JCVddsri",
+      redirect_uri: "https://ams1.n2bot.net/oauth2callback",
+      scope: "https://www.googleapis.com/auth/calendar",
+      response_type: "code",
+      approval_prompt: "force",
+      access_type: "offline"
+    }
+    query = q_hash.to_query
+    oauth_url = "https://accounts.google.com/o/oauth2/auth?#{query}"
+
+#    oauth_url = "https://accounts.google.com/o/oauth2/auth?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=#{scope}&response_type=code&approval_prompt=force&access_type=offline"
+
+    uri = URI(oauth_url)
     response = Net::HTTP.get(uri)
 
     Rails.logger.debug("get token response=#{response}")
